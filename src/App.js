6bba9d2438eb009745c70f71d3672568f9f6d3e8
Mohammad sadiqu <60215319+sadiquhasan@users.noreply.github.com>
2023-12-27 16:15:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import Table from "./component/Table";
 import Form from "./component/Form";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState(false);
+
   return (
     <div className="App">
-      <Table />
-      {/* <Form /> */}
+      <div className="flex justify-content-center align-items-center mt-4">
+        <buton
+          style={{ fontSize: "larger" }}
+          className="btn btn-link mx-3"
+          onClick={() => setTask(false)}
+        >
+          Form
+        </buton>
+        <buton
+          style={{ fontSize: "larger" }}
+          className="btn btn-link mx-3"
+          onClick={() => setTask(true)}
+        >
+          Task
+        </buton>
+      </div>
+
+      {task ? <Table /> : <Form />}
     </div>
   );
 }
